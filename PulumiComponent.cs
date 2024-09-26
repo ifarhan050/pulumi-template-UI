cc018eab4e2f6d90be66e59fed417de733fa70c0
Muhammad Farhan Bin Irfan <35654899+ifarhan050@users.noreply.github.com>
2024-09-26 18:58:44 +0500

    Create PulumiComponent.cs

---
+++ PulumiComponent.cs
@@ -0,0 +1,27 @@
+﻿using Pulumi;
+using PulumiGithub.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulumiGithub
+{
+    public class PulumiComponent
+    {
+        public PulumiComponent()
+        {
+                
+        }
+
+        public Dictionary<string,object?> CreateAzureResources()
+        {
+           Dictionary<string, object?> Output = new Dictionary<string, object?>();
+           //{{content}}
+           return Output;
+        }
+    }
+}
